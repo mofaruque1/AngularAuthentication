4c59b3a221d75903a0d8f68bd0bb3c278cb868a2
Md Omor Faruque <mofaruque1@gmail.com>
2018-05-19 14:04:21 -0400

    Angular Authentication  vxx

--- git_script.sh
+++ git_script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git add .
 git commit -m 'Angular Authentication  vxx'
-#!git remote add gitrepo https://github.com/mofaruque1/link
-git push -u gitrepo master
+git push -u gitrepo master -f
 clear
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 }
+//git remote add origin https://github.com/octocat/Spoon-Knife
