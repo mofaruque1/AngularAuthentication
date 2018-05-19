2185a6c02f2d984159cf390546766ef7fb646a2c
Md Omor Faruque <mofaruque1@gmail.com>
2018-05-19 14:03:03 -0400

    Angular Authentication  vxx

--- git_script.sh
+++ git_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
 git commit -m 'Angular Authentication  vxx'
-# git remote add gitrepo https://github.com/mofaruque1/link
+#!git remote add gitrepo https://github.com/mofaruque1/link
 git push -u gitrepo master
 clear
