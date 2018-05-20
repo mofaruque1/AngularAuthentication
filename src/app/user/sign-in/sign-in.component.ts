80409ff11f186acc07ac1ad5f0dabd8f50bf9a5d
Md Omor Faruque <mofaruque1@gmail.com>
2018-05-20 12:04:04 -0400

    Angular Authentication  vxx

--- src/app/user/sign-in/sign-in.component.ts
+++ src/app/user/sign-in/sign-in.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../../shared/user.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
@@ -7,13 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  isLoginError:boolean=false;
+  constructor(private userService:UserService, private router: Router) { }
 
   ngOnInit() {
   }
 
   OnSubmit(username:string,password:string){
+    this.userService.userAuthentication(username,password).subscribe((data:any)=>{
+      localStorage.setItem('userToken',data.access_token);
+      this.router.navigate(['/home']);
+    },
+  (error:HttpErrorResponse)=>{
+    this.isLoginError = true;
+    console.log(error.message);
     
+  })
   }
 
 }
